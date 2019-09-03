d908ba9d007d29194bc09d10d44e5ce99152097f
Miles Chew <mileschew42@gmail.com>
2019-09-02 22:40:35 -0700

    implement DetailActivity

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
@@ -15,12 +16,21 @@ public class DetailActivity extends AppCompatActivity {
     public static final String EXTRA_POSITION = "extra_position";
     private static final int DEFAULT_POSITION = -1;
 
+    private TextView akaTv;
+    private TextView descriptionTv;
+    private TextView ingredientsTv;
+    private TextView originTv;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
         ImageView ingredientsIv = findViewById(R.id.image_iv);
+        akaTv = findViewById(R.id.also_known_tv);
+        descriptionTv = findViewById(R.id.description_tv);
+        ingredientsTv = findViewById(R.id.ingredients_tv);
+        originTv = findViewById(R.id.origin_tv);
 
         Intent intent = getIntent();
         if (intent == null) {
@@ -43,9 +53,10 @@ public class DetailActivity extends AppCompatActivity {
             return;
         }
 
-        populateUI();
+        populateUI(sandwich);
         Picasso.with(this)
                 .load(sandwich.getImage())
+                .placeholder(R.drawable.sandwich_placeholder)
                 .into(ingredientsIv);
 
         setTitle(sandwich.getMainName());
@@ -56,7 +67,14 @@ public class DetailActivity extends AppCompatActivity {
         Toast.makeText(this, R.string.detail_error_message, Toast.LENGTH_SHORT).show();
     }
 
-    private void populateUI() {
-
+    private void populateUI(Sandwich sandwichData) {
+        if (sandwichData.getAlsoKnownAs() != null)
+            akaTv.setText(sandwichData.getAlsoKnownAs().toString());
+        if (sandwichData.getDescription() != null)
+            descriptionTv.setText(sandwichData.getDescription());
+        if (sandwichData.getIngredients() != null)
+            ingredientsTv.setText(sandwichData.getIngredients().toString());
+        if (sandwichData.getPlaceOfOrigin() != null)
+            originTv.setText(sandwichData.getPlaceOfOrigin());
     }
 }
--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -2,9 +2,68 @@ package com.udacity.sandwichclub.utils;
 
 import com.udacity.sandwichclub.model.Sandwich;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class JsonUtils {
 
+    private static final String KEY_NAME = "name";
+    private static final String KEY_NAME_MAIN = "mainName";
+    private static final String KEY_NAME_AKA = "alsoKnownAs";
+    private static final String KEY_PLACE_OF_ORIGIN = "placeOfOrigin";
+    private static final String KEY_DESCRIPTION = "description";
+    private static final String KEY_IMAGE = "image";
+    private static final String KEY_INGREDIENTS = "ingredients";
+
     public static Sandwich parseSandwichJson(String json) {
+        Sandwich s = new Sandwich();
+        try {
+            JSONObject sandwichJSON = new JSONObject(json);
+            JSONObject nameObject = sandwichJSON.getJSONObject(KEY_NAME);
+            s.setMainName(getOptString(nameObject, KEY_NAME_MAIN));
+
+            s.setImage(getOptString(sandwichJSON, KEY_IMAGE));
+            s.setAlsoKnownAs(jsonArrayToLst(getOptJSONArray(nameObject, KEY_NAME_AKA)));
+            s.setPlaceOfOrigin(getOptString(sandwichJSON, KEY_PLACE_OF_ORIGIN));
+            s.setIngredients(jsonArrayToLst(getOptJSONArray(sandwichJSON, KEY_INGREDIENTS)));
+            s.setDescription(getOptString(sandwichJSON, KEY_DESCRIPTION));
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return s;
+    }
+
+    private static String getOptString(JSONObject json, String key) {
+        try {
+            String value = json.getString(key);
+            if (!value.isEmpty())
+                return value;
+        } catch (JSONException ignored) { }
         return null;
+    }
+
+    private static JSONArray getOptJSONArray(JSONObject json, String key) {
+        try {
+            JSONArray array = json.getJSONArray(key);
+            if (array.length() > 0)
+                return array;
+        } catch (JSONException ignored) { }
+        return null;
+    }
+    
+    private static List<String> jsonArrayToLst(JSONArray array) {
+        if (array == null)
+            return null;
+        ArrayList<String> list = new ArrayList<>();
+        try {
+            for (int i = 0; i < array.length(); i++) {
+                list.add(array.get(i).toString());
+            }
+        } catch (JSONException ignored) { }
+        return list;
     }
 }
